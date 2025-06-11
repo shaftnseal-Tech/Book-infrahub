93fad8ea8ea630eff6fa407e5826a2f15f1dce96
Pritirekha23 <learnpriti23@gmail.com>
2025-06-11 10:17:50 +0530

    last commit

--- client/src/components/default.js
+++
@@ -1,4 +0,0 @@
-// export { default as Cart } from './Cart';
-export { default as Home } from './home/Home';
-// export { default as Header} from './Header';
-export { default as NotFound } from './NotFound';
